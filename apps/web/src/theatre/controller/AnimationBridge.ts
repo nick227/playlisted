7bb0f029c473cf5daf4ed926c6116188687e8841
Nick Rios <nicholas.jay.rios@gmail.com>
2026-06-30 05:47:49 -0500

    Fix missing resolveTheatreInitContext import in AnimationBridge.

--- apps/web/src/theatre/controller/AnimationBridge.ts
+++ apps/web/src/theatre/controller/AnimationBridge.ts
@@ -1,6 +1,7 @@
 import { AnimationFactory, IAnimation, AnimationContext } from '../core/IAnimation'
 import { staticFallbackFactory } from '../animations/staticFallback'
 import { theatreBreadcrumb } from './theatreBreadcrumbs'
+import { resolveTheatreInitContext } from './theatreInitContext'
 
 export type AnimationBridgeTrace = {
   presetId?: string
